bd3c12aca2ca543540b7980912d72e575f7c58d7
Raikoff <podchernin.ivan@gmail.com>
2022-03-17 14:27:01 +0300

    add interfece

--- Base.py
+++ Base.py
@@ -21,53 +21,100 @@
 
 
 
+import tkinter as tk
 
 
+window = tk.Tk()                              # интерфейс
+window.title('Справочник')
 
+frm_form = tk.Frame(relief=tk.SUNKEN, borderwidth=3)
+frm_form.pack()
 
-a = input('Фамилия: ')
-b = input('Имя: ')
-c = input('Телефон +')
-d = input('Описание: ')
-# # str(input('Фамилия: ')),str(input('Имя: ')),int(input('Телефон: ')),str(input('Описание: '))
+surname = tk.Label(master=frm_form,text="Фамилия: ")
+en_sur = tk.Entry(master=frm_form,width=50)
+surname.grid(row=0, column=0, sticky="e")
+en_sur.grid(row=0, column=1)
 
-if a.isalpha() == False:
-    exit()
-if b.isalpha() == False:
-    exit()
+name = tk.Label(master=frm_form,text="Имя: ")
+en_name = tk.Entry(master=frm_form,width=50)
+name.grid(row=1, column=0, sticky="e")
+en_name.grid(row=1, column=1)
 
-if c.isnumeric() == False:
-    exit()
+tel = tk.Label(master=frm_form,text="Телефон +: ")
+en_tel = tk.Entry(master=frm_form,width=50)
+tel.grid(row=2, column=0, sticky="e")
+en_tel.grid(row=2, column=1)
 
-def inputUsers(surname,name,tel,comment):
-    
-    list = [surname,name,tel,comment]        
-    list1 = ' '.join(list)
+com = tk.Label(master=frm_form,text="Описание: ")
+en_com = tk.Entry(master=frm_form,width=50)
+com.grid(row=3, column=0, sticky="e")
+en_com.grid(row=3, column=1)
+
+frm_enter = tk.Frame()
+frm_enter.pack()
+
+text_box = tk.Text()
+text_box.pack()
+
+frm_con = tk.Frame()
+frm_con.pack()
+
+btn_sav = tk.Button(master=frm_enter,text="Сохранить")
+btn_sav.pack(side=tk.RIGHT, padx=10, ipadx=10)
+
+btn_second = tk.Button(master=frm_con,text="Формат №2")
+btn_second.pack(side=tk.RIGHT, padx=10)
+
+btn_first = tk.Button(master=frm_con,text="Формат №1")
+btn_first.pack(side=tk.RIGHT, padx=10, ipadx=10)
+
+# if en_sur.isalpha() == False:
+#     exit()
+# if en_name.isalpha() == False:
+#     exit()
+#
+# if en_tel.isnumeric() == False:
+#     exit()
+
+def inputUsers(en_sur,en_name,en_tel,en_com):
+    list = [en_sur,en_name,en_tel,en_com]
+    list1 = ' '.join(map(str,list))
     return list1
 
-def inputUsers2(surname,name,tel,comment):     
-    
-    list = [surname,name,tel,comment]        
-    list1 = '\n'.join(list)
+btn_sav.bind('<Button-1>', lambda event: inputUsers(en_sur,en_name,en_tel,en_com))
+
+def inputUsers2(en_sur,en_name,en_tel,en_com):
+
+    list = [en_sur,en_name,en_tel,en_com]
+    list1 = '\n'.join(map(str,list))
     return list1
-    
-with open('file1.csv', 'a') as data:
-    data.writelines(inputUsers(a,b,c,d))
+
+btn_sav.bind('<Button-1>',lambda event: inputUsers2(en_sur,en_name,en_tel,en_com))
+
+
+with open('file1.txt', 'a') as data:
+    data.writelines(inputUsers(en_sur,en_name,en_tel,en_com))
     data.write('\n')
 
-with open('file2.csv', 'a') as data:
-    data.write('\n')
-    data.writelines(inputUsers2(a,b,c,d))
-    
-while format != 1 or format != 2:
-    format = int(input('Выбирите формат вывода 1 или 2?: '))
-    if format == 1:
-        path = 'file1.csv'
-        data = open(path, 'r')
-        for line in data:
-            print(line)
-    elif format == 2:
-        path = 'file2.csv'
-        data = open(path, 'r')
-        for line in data:
-            print(line)
+with open('file2.txt', 'a') as data:
+    data.writelines('\n')
+    data.write(inputUsers2(en_sur,en_name,en_tel,en_com))
+
+# def click_format():                                           # зависает программа
+#     while format != btn_first or format != btn_second:
+#         if format == btn_first:
+#             path = 'file1.txt'
+#             data = open(path, 'r')
+#             for line in data:
+#                 text_box.get(line)
+#         elif format == btn_second:
+#             path = 'file2.txt'
+#             data = open(path, 'r')
+#             for line in data:
+#                 text_box.get(line)
+
+# btn_second.bind('<Button-1>',lambda event: click_format())
+
+# btn_first.bind('<Button-1>',lambda event: click_format())
+
+window.mainloop()
